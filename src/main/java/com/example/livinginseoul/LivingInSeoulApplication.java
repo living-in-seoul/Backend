c0289cb818d9d2a8a8978ae26cf518755e5d7d3a
chaewon <cwlee4633@naver.com>
2023-08-01 11:57:48 +0900

    git init

---
+++ src/main/java/com/example/livinginseoul/LivingInSeoulApplication.java
@@ -0,0 +1,13 @@
+package com.example.livinginseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingInSeoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivingInSeoulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/livinginseoul/LivingInSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livinginseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivingInSeoulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
